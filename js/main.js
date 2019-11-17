3742d8536071d472d616be4c014fbe42fba450eb
ahmsalah <50025978+ahmsalah@users.noreply.github.com>
2019-11-17 15:59:10 +0200

    Make the typed animation an infinite loop

--- js/main.js
+++ js/main.js
@@ -1,64 +1,68 @@
-$(window).on("load", function() {
-  $(".loader").fadeOut(50);
-})
+$(window).on('load', function() {
+	$('.loader').fadeOut(50);
+});
 
-$(document).ready(function () {
-  /**
+$(document).ready(function() {
+	/**
   |--------------------------------------------------
   |  Navigation
   |--------------------------------------------------
   */
 
-  const navList = document.querySelector(".js-nav-list");
-  const navCheckbox = document.querySelector(".js-nav-checkbox");
+	const navList = document.querySelector('.js-nav-list');
+	const navCheckbox = document.querySelector('.js-nav-checkbox');
 
-      
-  function handleMenuClick(event) {
-    if (event.target instanceof HTMLAnchorElement) {
-      navCheckbox.checked = false;
-    }
-  }
-      
-  navList.addEventListener('click', handleMenuClick);
+	function handleMenuClick(event) {
+		if (event.target instanceof HTMLAnchorElement) {
+			navCheckbox.checked = false;
+		}
+	}
 
-  /**
+	navList.addEventListener('click', handleMenuClick);
+
+	/**
   |--------------------------------------------------
   | Animate Scrolling
   |--------------------------------------------------
   */
 
-  $('a[href*="#"]').on('click', function (e) {
-    e.preventDefault(); 
-    const targetElement = $(this).attr("href");
-    const targetPosition = $(targetElement).offset().top
-    $('html, body').animate({ scrollTop: (targetElement === '#contact' ? targetPosition + 60 : targetPosition + 20) }, 1500, "easeInOutExpo")
-  });
-  
-  /**
+	$('a[href*="#"]').on('click', function(e) {
+		e.preventDefault();
+		const targetElement = $(this).attr('href');
+		const targetPosition = $(targetElement).offset().top;
+		$('html, body').animate(
+			{ scrollTop: targetElement === '#contact' ? targetPosition + 60 : targetPosition + 20 },
+			1500,
+			'easeInOutExpo'
+		);
+	});
+
+	/**
   |--------------------------------------------------
   | Animate on Scroll
   |--------------------------------------------------
   */
 
-  AOS.init({
-    easing: 'ease',
-    once: true,
-    duration: 1800
-  })
+	AOS.init({
+		easing: 'ease',
+		once: true,
+		duration: 1800
+	});
 
-  /**
+	/**
   |--------------------------------------------------
   | Typed.js
   |--------------------------------------------------
   */
 
-
-  const typed = new Typed(".js-typed", {
-    strings: ["A Front-end Web Developer based in Cairo."],
-    typeSpeed: 70,
-    startDelay: 2700,
-    showCursor: false
-  })
-
+	const typed = new Typed('.js-typed', {
+		strings: [ 'A Front-end Web Developer based in Cairo.' ],
+		typeSpeed: 70,
+		startDelay: 2700,
+		showCursor: false,
+		loop: true,
+		loopCount: Infinity,
+		backDelay: 700,
+		backSpeed: 20
+	});
 });
-
